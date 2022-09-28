99efcbdd4c55c057de46d3a9c9474cfb1b4fce2e
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-09-28 15:38:06 -0300

    Initial commit

---
+++ src/components/Table.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+  render() {
+    return (
+      <div>Table</div>
+    );
+  }
+}
+
+export default Table;
---
+++ src/components/WalletForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WalletForm extends Component {
+  render() {
+    return (
+      <div>WalletForm</div>
+    );
+  }
+}
+
+export default WalletForm;
